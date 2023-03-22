d4601b5c9be05c2e3250e1fe5de5fa2d6acbcf8d
Meacha1 <meachattd@gmail.com>
2023-03-22 01:52:13 -0700

    filestorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-
+import os
 
 storage = FileStorage()
 storage.reload()
