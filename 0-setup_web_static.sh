f9b78733545708c8be0ebd4e1688cb73ec2ef3b0
Meacha1 <meachattd@gmail.com>
2023-04-06 10:19:39 -0700

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,57 @@
 #!/usr/bin/env bash
-# Install nginx if not already installed
-if [ ! -x "$(command -v nginx)" ]; then
-    sudo apt-get update
-    sudo apt-get install -y nginx
+# Installs, configures, and starts the web server
+SERVER_CONFIG="server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	server_name _;
+	index index.html index.htm;
+	error_page 404 /404.html;
+	add_header X-Served-By \$hostname;
+	location / {
+		root /var/www/html/;
+		try_files \$uri \$uri/ =404;
+	}
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+		try_files \$uri \$uri/ =404;
+	}
+	if (\$request_filename ~ redirect_me) {
+		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
+	}
+	location = /404.html {
+		root /var/www/error/;
+		internal;
+	}
+}"
+HOME_PAGE="<!DOCTYPE html>
+<html lang='en-US'>
+	<head>
+		<title>Hello Meacha</title>
+	</head>
+	<body>
+		<h1>Welcome Meacha to AirBnB!</h1>
+	<body>
+</html>
+"
+# shellcheck disable=SC2230
+if [[ "$(which nginx | grep -c nginx)" == '0' ]]; then
+    apt-get update
+    apt-get -y install nginx
 fi
+mkdir -p /var/www/html /var/www/error
+chmod -R 755 /var/www
+echo 'Hello World!' > /var/www/html/index.html
+echo -e "Ceci n\x27est pas une page" > /var/www/error/404.html
 
-# Create necessary folders if they don't exist
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-
-# Create fake HTML file
-sudo echo "Hello World" > /data/web_static/releases/test/index.html
-
-# Create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership of /data/ to ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update nginx configuration
-sudo sed -i '/^http {/a \    server {\n        listen 80 default_server;\n        listen [::]:80 default_server;\n        location /hbnb_static {\n            alias /data/web_static/current/;\n        }\n    }' /etc/nginx/nginx.conf
-
-# Restart nginx
-sudo service nginx restart
-
-exit 0
+mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo -e "$HOME_PAGE" > /data/web_static/releases/test/index.html
+[ -d /data/web_static/current ] && rm -rf /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data
+bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
+ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+if [ "$(pgrep -c nginx)" -le 0 ]; then
+	service nginx start
+else
+	service nginx restart
+fi
