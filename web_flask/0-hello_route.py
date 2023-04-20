2dfadc22e722e0c0c24d82af54f21ab27eb95adf
Meacha1 <meachattd@gmail.com>
2023-04-20 09:02:14 -0700

    hello

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-
+'''A script that starts a Flask web application.
+'''
 from flask import Flask
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def main():
+@app.route("/", strict_slashes=False)
+def hello():
+    '''The home'''
     return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
