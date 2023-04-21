5d3eae8b641495e2e47b80a4dcdbb2679fc87cbc
Meacha1 <meachattd@gmail.com>
2023-04-21 01:18:12 -0700

    c

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -16,7 +16,7 @@ def hello():
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     '''The hbnb page'''
-    return "HBNB!"
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
