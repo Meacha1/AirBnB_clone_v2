fc191cf60360212a44ee640d1d91aea227bc36f7
Meacha1 <meachattd@gmail.com>
2023-04-21 08:33:03 -0700

    reset

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 import os
-import sys
-sys.path.append("/AirBnB_clone_v2/models/engine/")
 
-from db_storage import DBStorage
-from file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
 
 storage = DBStorage() if os.getenv(
     'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
