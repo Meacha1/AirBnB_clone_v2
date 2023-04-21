025fd19dcefc85b276e1a45656b5fc453b7a78b2
Meacha1 <meachattd@gmail.com>
2023-04-21 02:22:07 -0700

    template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,7 +41,7 @@ def number(n):
 @app.route('number_template/<int:n>', strict_slashes=False)
 def tmplate(n):
     '''The template page'''
-    return render_template('5-number.html', n=n)
+    return render_template('/template/5-number.html', n=n)
 
 
 if __name__ == "__main__":
