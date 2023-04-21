a41653cb93b532998a02bff54d99db1acf5d4151
Meacha1 <meachattd@gmail.com>
2023-04-21 08:35:12 -0700

    reset

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class to manage database storage for hbnb clone"""
 import os
-import sys
-sys.path.append("../..")
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 import urllib.parse
@@ -89,4 +87,4 @@ class DBStorage:
 
     def close(self):
         """Closes the storage engine."""
-        self.__session.remove()
+        self.__session.close()
