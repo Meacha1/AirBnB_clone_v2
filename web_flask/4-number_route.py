6b41b3b87dcf0c84f2648996f9b67084c0a1d748
Meacha1 <meachattd@gmail.com>
2023-04-21 01:55:47 -0700

    number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+'''A script that starts a Flask web application.
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''The home page'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''The hbnb page'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    '''The C page'''
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/(<text>)', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'})
+def python(text):
+    '''The python page'''
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('number/<n>', strict_slashes=False)
+def number(n):
+    '''The number page'''
+    if n.isdigit():
+        return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
