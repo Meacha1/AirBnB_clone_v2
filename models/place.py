aab87ae6e12ef550ce9d97ce64b4701f6eb99995
Meacha1 <meachattd@gmail.com>
2023-04-22 01:35:55 -0700

    change

--- models/place.py
+++ models/place.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+import os
+from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.amenity import Amenity
 
+place_amenity = Table(
+    'place_amenity',
+    Base.metadata,
+    Column(
+        'place_id',
+        String(60),
+        ForeignKey('places.id'),
+        nullable=False,
+        primary_key=True
+    ),
+    Column(
+        'amenity_id',
+        String(60),
+        ForeignKey('amenities.id'),
+        nullable=False,
+        primary_key=True
+    )
+)
 
 class Place(BaseModel):
     """ A place to stay """
